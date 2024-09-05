cabd25a071c63b75d85aa16d503b5c56d5b44065
takeshiumemoto <takesiumemoto@gmail.com>
2024-09-05 15:15:01 +0900

    java

---
+++ java1/Hello.java
@@ -0,0 +1,6 @@
+class Hello{
+    public static void main(String[] args){
+        String message = "Hello";
+        System.out.println(message);
+    }
+}
---
+++ null/null.java
@@ -0,0 +1,8 @@
+class Hello{
+    public static void main(String[]args){
+        System.out.println("Good morning");
+        System.out.println("Good afternonn");
+        System.out.println("Good evening");
+
+    }
+}
