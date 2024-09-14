c0ba0b78e35fed507dcb27ec0579870eb62af29d
takeshiumemoto <takesiumemoto@gmail.com>
2024-09-14 20:50:24 +0900

    java

---
+++ Bunki6/Bunki6.java
@@ -0,0 +1,16 @@
+package Bunki6;
+
+public class Bunki6 {
+    public static void main(String[] args) {
+        int a = 1;
+        String message = switch(a){
+            case 1: yield "Goodmorinig";
+            case 2: yield "Goodafternonn";
+            case 3: yield "Goodevening";
+            default: yield "Good night";
+        };
+        System.out.println(message);
+    }
+    
+}
+
